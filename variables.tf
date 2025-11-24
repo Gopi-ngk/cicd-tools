690896b02180d7e3190c1e94d2f0d4c28a84b7ca
Gopi <gopikuchi1827@gmail.com>
2025-11-24 19:51:11 +0530

    Jenkins

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "gopinath.store"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z00345272KTJ7HVI6EF6V"
+  description = "description"
+}
